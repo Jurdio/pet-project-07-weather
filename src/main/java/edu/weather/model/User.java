560843d9628d5117faec2d27f5ebfeb6913d729f
Dmytro <dmytro.kupets@gmail.com>
2024-02-29 18:18:51 +0200

    feat(model): add User model

---
+++ src/main/java/edu/weather/model/User.java
@@ -0,0 +1,26 @@
+package edu.weather.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String login;
+
+    @Column(name = "userPassword")
+    public String password;
+}
