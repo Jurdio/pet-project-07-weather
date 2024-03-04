4b1a46a46da5360a12eb9bac0bc5fa6219c46719
Dmytro <dmytro.kupets@gmail.com>
2024-03-04 14:56:09 +0200

    feat: add HibernateUtil.java for optimize session factory

---
+++ src/main/java/edu/weather/util/HibernateUtil.java
@@ -0,0 +1,26 @@
+package edu.weather.util;
+
+import lombok.Getter;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    @Getter
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+}
