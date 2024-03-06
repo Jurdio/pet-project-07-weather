5733d31ec7352362c562d13e927b5b7c9a00d37b
Dmytro <dmytro.kupets@gmail.com>
2024-03-06 20:43:14 +0200

    feat: implement interface in SessionRepository.java

--- src/main/java/edu/weather/domain/repository/SessionRepository.java
+++ src/main/java/edu/weather/domain/repository/SessionRepository.java
@@ -1,16 +1,27 @@
 package edu.weather.domain.repository;
 
+
 import edu.weather.domain.model.entity.Session;
+import edu.weather.exception.DataAccessException;
+import edu.weather.util.HibernateUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public class SessionRepository implements CrudRepository<Session, UUID>{
+@Slf4j
+public class SessionRepository implements CrudRepository<Session, UUID> {
 
     @Override
     public Optional<Session> findById(UUID id) {
-        return Optional.empty();
+        try (org.hibernate.Session openSession = HibernateUtil.getSessionFactory().openSession()) {
+            Session session = openSession.get(Session.class, id);
+            return Optional.ofNullable(session);
+        } catch (Exception e) {
+            log.error("Error while findById", e);
+            throw new DataAccessException("Error while retrieving session by ID");
+        }
     }
 
     @Override
